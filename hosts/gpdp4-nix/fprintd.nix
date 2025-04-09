c35408c0be4f31cc0d81d543d6eba924f780343d
Cristian Stamateanu <cristids@gmail.com>
2025-04-09 14:42:02 +0300

    try with tod

--- hosts/gpdp4-nix/fprintd.nix
+++ hosts/gpdp4-nix/fprintd.nix
@@ -3,4 +3,7 @@
     # GPD Pocket 4 - FocalTech Fingerprint Reader (2808:0752)
     SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="0752", MODE="0660", GROUP="plugdev", TAG+="uaccess"
   '';
+
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-focaltech-2808-a658;
 }
