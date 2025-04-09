5900e36a2cfe6d3cf22951ef7e4f3266c63b8ae6
Cristian Stamateanu <cristids@gmail.com>
2025-04-09 14:54:24 +0300

    fix 2

--- hosts/gpdp4-nix/fprintd.nix
+++ hosts/gpdp4-nix/fprintd.nix
@@ -5,6 +5,14 @@
     SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="0752", MODE="0660", GROUP="plugdev", TAG+="uaccess"
   '';
 
+  environment.systemPackages = with pkgs; [
+    # fprintd
+    fprintd
+    libfprint-focaltech-2808-a658
+  ];
+  # fprintd configuration
+
+
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-focaltech-2808-a658;
 }
