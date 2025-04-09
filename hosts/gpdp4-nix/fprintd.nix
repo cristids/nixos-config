2f505d438d503ff79809e01add6e7ca47e704d5d
Cristian Stamateanu <cristids@gmail.com>
2025-04-09 14:29:12 +0300

    modify the rule

--- hosts/gpdp4-nix/fprintd.nix
+++ hosts/gpdp4-nix/fprintd.nix
@@ -1,9 +1,6 @@
 {
   services.udev.extraRules = ''
-    # FocalTech fingerprint sensor
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="*", ATTRS{dev}=="*", \
-      TEST=="power/control", ATTR{power/control}="auto", MODE="0660", GROUP="plugdev"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="*", \
-      ENV{LIBFPRINT_DRIVER}="FocalTech Systems Co., Ltd Fingerprint"
+    # GPD Pocket 4 - FocalTech Fingerprint Reader (2808:0752)
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="0752", MODE="0660", GROUP="plugdev", TAG+="uaccess"
   '';
 }
