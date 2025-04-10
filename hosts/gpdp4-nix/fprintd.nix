b93f1fd14052a7318bd70921cdefa288ab2f0506
Cristian Stamateanu <cristids@gmail.com>
2025-04-10 18:20:48 +0300

    before moving to redhat version

--- hosts/gpdp4-nix/fprintd.nix
+++ hosts/gpdp4-nix/fprintd.nix
@@ -54,16 +54,39 @@ let
   # Inline the custom package
   libfprintTod = pkgs.callPackage ../../pkgs/libfprint-tod/package.nix {};
 in {
+  services.udev.extraRules = ''
+    # GPD Pocket 4 - FocalTech Fingerprint Reader (2808:0752)
+    # SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="0752", MODE="0660", GROUP="plugdev", TAG+="uaccess"
+    
+    # FocalTech Systems Co., Ltd Fingerprint
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="*", ATTRS{dev}=="*", TEST=="power/control", ATTR{power/control}="auto", MODE="0660", GROUP="plugdev"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="*", ENV{LIBFPRINT_DRIVER}="FocalTech Systems Co., Ltd Fingerprint"
+  '';
+
   # Enable fprintd with tod driver
-  services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = libfprintTod;
+  services.fprintd = {
+    enable = true;
+    package = pkgs.fprintd.override {
+      libfprint = libfprintTod;
+    };
+    # package = pkgs.fprintd-tod;
+    # tod = {
+    #   enable = true;
+    #   driver = libfprintTod;
+    # };
+    # Optional: Enable fprintd for other drivers if needed
+    # enable = true;
+    # drivers = [ "libfprint-focaltech-2808-a658" ];
+  };
+  # services.fprintd.enable = true;
+  # services.fprintd.tod.enable = true;
+  # services.fprintd.tod.driver = libfprintTod;
 
   # Include the package in systemPackages for CLI access if needed
-  environment.systemPackages = [ libfprintTod ];
+  # environment.systemPackages = [ pkgs.fprintd libfprintTod ];
 
   # Ensure udev rules are active
-  services.udev.packages = [ libfprintTod ];
+  services.udev.packages = [ pkgs.gusb libfprintTod ];
 
   # Optional: Allow unfree for this host if you haven't globally
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
--- pkgs/libfprint-tod/package.nix
+++ pkgs/libfprint-tod/package.nix
@@ -1,38 +1,131 @@
-{ lib, stdenv, fetchurl, dpkg }:
+{
+  stdenv,
+  lib,
+  fetchurl,
+  meson,
+  ninja,
+  gobject-introspection,
+  cpio,
+  glib,
+  gusb,
+  pixman,
+  libgudev,
+  nss,
+  libfprint,
+  cairo,
+  pkg-config,
+  autoPatchelfHook,
+  makePkgconfigItem,
+  copyPkgconfigItems,
+  dpkg,
+  gtk-doc,
+  docbook-xsl-nons,
+  docbook_xml_dtd_43,
+  
+}:
+let
+  # The provided `.so`'s name in the binary package we fetch and unpack
+  libso = "libfprint-2.so.2.0.0";
+in
+
 
 stdenv.mkDerivation rec {
-  pname = "libfprint-2-2-tod";
+  pname = "libfprint-focaltech-driver";
+  # pname = "libfprint-2-2-tod";
   version = "1.94.4-tod1";
+  NIX_CFLAGS_LINK = "-lgusb";
 
   src = fetchurl {
     url = "https://github.com/ftfpteams/focaltech-linux-fingerprint-driver/raw/refs/heads/main/Ubuntu_Debian/x86/libfprint-2-2_1.94.4+tod1-0ubuntu1~22.04.2_amd64_20250219.deb";
     sha256 = "06j0i02hx98q2f4x50y2h2di87h0ihvi0ky03xg0f62pd2xmx37y";
   };
 
-  nativeBuildInputs = [ dpkg ];
+  nativeBuildInputs = [ 
+    dpkg
+    pkg-config 
+    autoPatchelfHook
+    copyPkgconfigItems
+    meson
+    ninja
+    gobject-introspection
+  ];
+
+  buildInputs = [
+    stdenv.cc.cc
+    glib
+    gusb
+    pixman
+    nss
+    libgudev
+    libfprint
+    cairo
+  ];
+
+# Optional, to debug linking:
+  preConfigure = ''
+    echo "GUSB flags:"
+    pkg-config --libs --cflags gusb || true
+  '';
+
+
+  mesonFlags = [
+    # "-Dudev_rules_dir=${placeholder "out"}/lib/udev/rules.d"
+    # # Include virtual drivers for fprintd tests
+    # "-Ddrivers=all"
+    # "-Dudev_hwdb_dir=${placeholder "out"}/lib/udev/hwdb.d"
+    "-Dsystemd=false"  
+    "-Dintrospection=false"
+  ];
 
   unpackPhase = ''
+    runHook preUnpack
+
     dpkg-deb -x $src .
+
+    runHook postUnpack
   '';
+
+  patchPhase = ''
+  substituteInPlace meson.build \
+    --replace "dependency('libfprint-2'," "dependency('libfprint-2', required: true), dependency('gusb', required: true),"
+'';
+
+  # custom pkg-config based on libfprint's pkg-config
+  pkgconfigItems = [
+    (makePkgconfigItem rec {
+      name = "libfprint-2";
+      inherit version;
+      inherit (meta) description;
+      cflags = [ "-I${variables.includedir}/libfprint-2" ];
+      libs = [
+        "-L${variables.libdir}"
+        "-lfprint-2"
+      ];
+      variables = rec {
+        prefix = "${placeholder "out"}";
+        includedir = "${prefix}/include";
+        libdir = "${prefix}/lib";
+      };
+    })
+  ];
+
 
   installPhase = ''
-    echo ">>> Installing libfprint-tod"
-    find . -type f
+    runHook preInstall
 
-    mkdir -p $out/lib
-    cp -v usr/lib/x86_64-linux-gnu/libfprint-2.so.* $out/lib/
+    install -Dm444 usr/lib/x86_64-linux-gnu/${libso} -t $out/lib
 
-    # Install udev rules from lib/udev/rules.d/
-    if [ -d lib/udev/rules.d ]; then
-      echo ">>> Found udev rules in lib/udev/rules.d"
-      mkdir -p $out/lib/udev/rules.d
-      cp -v lib/udev/rules.d/* $out/lib/udev/rules.d/
-    fi
+    # create this symlink as it was there in libfprint
+    ln -s -T $out/lib/${libso} $out/lib/libfprint-2.so
+    ln -s -T $out/lib/${libso} $out/lib/libfprint-2.so.2
 
-    echo ">>> Done installing"
+    # get files from libfprint required to build the package
+    cp -r ${libfprint}/lib/girepository-1.0 $out/lib
+    cp -r ${libfprint}/include $out
+
+    runHook postInstall
   '';
 
-  driverPath = "/lib";
 
   meta = with lib; {
     description = "FocalTech libfprint driver from .deb";
