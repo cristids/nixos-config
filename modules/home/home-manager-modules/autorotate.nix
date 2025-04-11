39f70614b6439dce1b3456e8c2dad5300d7577a3
Cristian Stamateanu <cristids@gmail.com>
2025-04-11 22:45:01 +0300

    fix sleep 5

--- modules/home/home-manager-modules/autorotate.nix
+++ modules/home/home-manager-modules/autorotate.nix
@@ -9,18 +9,23 @@ in {
     Unit = {
       Description = "Auto screen rotation using monitor-sensor";
       After = [ "graphical-session.target" ];
+      Requires = [ "graphical-session.target" ];
     };
 
-     Service = {
+    Service = {
+      Type = "simple";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
       ExecStart = "${autorotateScript}/bin/autorotate";
       Restart = "on-failure";
+
+      # Optional: If DISPLAY/WAYLAND is still flaky, you can inject them manually
       # Environment = [
       #   "DISPLAY=:0"
-      #   # "XDG_RUNTIME_DIR=/run/user/${toString config.home.user.uid}"
       #   "WAYLAND_DISPLAY=wayland-0"
+      #   "XDG_RUNTIME_DIR=/run/user/${toString config.home.user.uid}"
       # ];
     };
-    
+
     Install = {
       WantedBy = [ "default.target" ];
     };
