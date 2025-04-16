6b850621a993ad8d49b49e30ed9466e6a44346d5
Cristian Stamateanu <cristids@gmail.com>
2025-04-16 12:25:07 +0300

    Add custom azuredatastudio

--- modules/home/home-manager-modules/packages-unstable.nix
+++ modules/home/home-manager-modules/packages-unstable.nix
@@ -1,10 +1,13 @@
 { config, pkgs, unstable, ... }:
-
+let
+  customAzureDataStudio = unstable.callPackage ../../../pkgs/azuredatastudio/package.nix {};
+in {
 {
   home.packages = with unstable;[
     _1password-gui
     affine
-    azuredatastudio
+    #azuredatastudio
+    customAzureDataStudio
     vscode
  
   ];
