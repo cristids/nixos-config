95fe0976d1cd43af4aebcb56e1d0fea46142f753
CristiDS <cristids@yahoo.com>
2025-05-01 21:31:14 +0300

    add helix

--- modules/home/home-manager-modules/helix.nix
+++ modules/home/home-manager-modules/helix.nix
@@ -1,27 +1,62 @@
-programs.helix = {
-  enable = true;
-  settings = {
-    theme = "autumn_night_transparent";
-    editor.cursor-shape = {
-      normal = "block";
-      insert = "bar";
-      select = "underline";
+{ pkgs, ... }: {
+  programs.helix = {
+    enable = true;
+    extraPackages = with pkgs; [ basedpyright ];
+    settings = {
+      theme = "autumn_night_transparent";
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+    };
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }
+      {
+        name = "python";
+        scope = "source.python";
+        auto-format = true;
+        indent = {
+          tab-width = 4;
+          unit = " ";
+        };
+        formatter = {
+          command = "${pkgs.black}/bin/black";
+          args = [ "-" "-q" ];
+        };
+        debugger = {
+          name = "debugpy";
+          transport = "stdio";
+          command = "python";
+          args = [ "-m" "debugpy.adapter" ];
+          templates = [{
+            name = "source";
+            request = "launch";
+            completion = [{
+              name = "entrypoint";
+              completion = "filename";
+              default = ".";
+            }];
+            args = {
+              mode = "debug";
+              program = "{0}";
+            };
+          }];
+        };
+        roots = [ "pyproject.toml" "pyrightconfig.json" "Poetry.lock" ];
+        language-servers =
+          [ { name = "basedpyright"; } { name = "ruff-lsp"; } ];
+      }
+    ];
+    themes = {
+      autumn_night_transparent = {
+        "inherits" = "autumn_night";
+        "ui.background" = { };
+      };
     };
   };
-  languages.language = [{
-    name = "nix";
-    auto-format = true;
-    formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-  }
-    {
-      name = "python";
-      auto-format = true;
-    }
-  ];
-  themes = {
-    autumn_night_transparent = {
-      "inherits" = "autumn_night";
-      "ui.background" = { };
-    };
-  };
-};
+}
